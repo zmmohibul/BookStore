95f9bd80fe5f4a9d8b3728aad18ff4aa7e4e4e1d
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-11 12:01:57 +0600

    Client Refactor 2

--- client/src/app/authentication/authentication.module.ts
+++ client/src/app/authentication/authentication.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthenticationRoutingModule, FormsModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
