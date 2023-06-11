7885e55ef54a8220ce708d2c7d85c3822a5e374a
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-11 15:21:41 +0600

    Update NavBar

---
+++ client/src/app/admin/store-manager/store-manager.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StoreManagerRoutingModule } from './store-manager-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreManagerRoutingModule
+  ]
+})
+export class StoreManagerModule { }
