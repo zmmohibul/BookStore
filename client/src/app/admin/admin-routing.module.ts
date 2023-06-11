7e76e5cd73fc28ee1a330868a27f087190782b5b
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-11 12:04:25 +0600

    Client Refactor 3

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'admin', component: AdminHomeComponent },
   {
     path: 'auth',
     loadChildren: () =>
       import('./authentication/authentication.module').then(
         (module) => module.AuthenticationModule
       ),
+  },
+
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((module) => module.AdminModule),
   },
 ];
 
