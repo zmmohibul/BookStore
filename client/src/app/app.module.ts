aafbd478d4c37f7adb40f072c02aeba9a2cf97f5
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-11 12:24:46 +0600

    Client Refactor 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AdminHomeComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
