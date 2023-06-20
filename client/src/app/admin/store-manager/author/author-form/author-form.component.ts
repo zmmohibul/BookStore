36f8cb9b8388b889faf9bc55d87c2aa2f4d9ddcc
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-21 03:07:38 +0600

    Client - Author Picture Upload

--- client/src/app/admin/store-manager/author/author-form/author-form.component.ts
+++ client/src/app/admin/store-manager/author/author-form/author-form.component.ts
@@ -1,7 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthorsService } from '../../../../services/authors.service';
 import { CreateAuthorModel } from '../../../../models/author/createAuthorModel';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../../../environments/environment';
+import { User } from '../../../../models/user';
+import { AuthenticationService } from '../../../../services/authentication.service';
+import { take } from 'rxjs';
+import { Author } from '../../../../models/author/author';
 
 @Component({
   selector: 'app-author-form',
@@ -11,13 +17,32 @@ import { CreateAuthorModel } from '../../../../models/author/createAuthorModel';
 export class AuthorFormComponent implements OnInit {
   @Input() authorForm: FormGroup = new FormGroup({});
 
+  uploader: FileUploader | undefined;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+
+  user: User | undefined;
+  author: Author | undefined;
+
+  uploadedPictureUrl = '';
+
   constructor(
     private formBuilder: FormBuilder,
-    private authorService: AuthorsService
-  ) {}
+    private authorService: AuthorsService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) {
+          this.user = user;
+        }
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeUploader();
   }
 
   initializeForm() {
@@ -27,6 +52,33 @@ export class AuthorFormComponent implements OnInit {
     });
   }
 
+  fileOverBase(e: any) {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: `${this.baseUrl}/authors/${this.author?.id}/add-picture`,
+      authToken: 'Bearer ' + this.user?.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 30 * 1024 * 1024,
+    });
+
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.uploadedPictureUrl = photo.url;
+      }
+    };
+  }
+
   onSubmit() {
     console.log(this.authorForm.value);
     if (this.authorForm.valid) {
@@ -34,6 +86,11 @@ export class AuthorFormComponent implements OnInit {
       this.authorService.createAuthor(authorModel).subscribe({
         next: (res) => {
           console.log(res);
+          this.author = res;
+          this.uploader?.setOptions({
+            url: `${this.baseUrl}/authors/${this.author?.id}/add-picture`,
+          });
+          this.uploader?.uploadAll();
         },
       });
     }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { InputComponent } from './input/input.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { ToastrModule } from 'ngx-toastr';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FooterComponent} from './footer/footer.component';
+import {MdbAccordionModule} from 'mdb-angular-ui-kit/accordion';
+import {MdbCarouselModule} from 'mdb-angular-ui-kit/carousel';
+import {MdbCheckboxModule} from 'mdb-angular-ui-kit/checkbox';
+import {MdbCollapseModule} from 'mdb-angular-ui-kit/collapse';
+import {MdbDropdownModule} from 'mdb-angular-ui-kit/dropdown';
+import {MdbFormsModule} from 'mdb-angular-ui-kit/forms';
+import {MdbModalModule} from 'mdb-angular-ui-kit/modal';
+import {MdbPopoverModule} from 'mdb-angular-ui-kit/popover';
+import {MdbRadioModule} from 'mdb-angular-ui-kit/radio';
+import {MdbRangeModule} from 'mdb-angular-ui-kit/range';
+import {MdbRippleModule} from 'mdb-angular-ui-kit/ripple';
+import {MdbScrollspyModule} from 'mdb-angular-ui-kit/scrollspy';
+import {MdbTabsModule} from 'mdb-angular-ui-kit/tabs';
+import {MdbTooltipModule} from 'mdb-angular-ui-kit/tooltip';
+import {MdbValidationModule} from 'mdb-angular-ui-kit/validation';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {InputComponent} from './input/input.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {FileUploadModule} from "ng2-file-upload";
 
 @NgModule({
   declarations: [FooterComponent, InputComponent],
@@ -41,6 +41,8 @@ import { ToastrModule } from 'ngx-toastr';
     MdbTabsModule,
     MdbTooltipModule,
     MdbValidationModule,
+
+    FileUploadModule,
 
     MatPaginatorModule,
 
@@ -68,6 +70,8 @@ import { ToastrModule } from 'ngx-toastr';
 
     MatPaginatorModule,
 
+    FileUploadModule,
+
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
@@ -76,4 +80,5 @@ import { ToastrModule } from 'ngx-toastr';
     InputComponent,
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+}
