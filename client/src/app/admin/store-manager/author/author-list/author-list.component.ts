b7c5b16d5044ed907abfcd69fd4897239ad6de4c
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-21 21:22:53 +0600

    Client - Author Pagination Works

--- client/src/app/admin/store-manager/author/author-list/author-list.component.ts
+++ client/src/app/admin/store-manager/author/author-list/author-list.component.ts
@@ -1,35 +1,39 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthorsService} from "../../../../services/authors.service";
-import {Author} from "../../../../models/author/author";
-import {PaginatedList} from "../../../../models/paginatedList";
-import {PageEvent} from "@angular/material/paginator";
+import {AuthorsService} from '../../../../services/authors.service';
+import {Author} from '../../../../models/author/author';
+import {PaginatedList} from '../../../../models/paginatedList';
+import {PageEvent} from '@angular/material/paginator';
+import {PaginationParams} from '../../../../models/paginationParams';
 
 @Component({
   selector: 'app-author-list',
   templateUrl: './author-list.component.html',
-  styleUrls: ['./author-list.component.scss']
+  styleUrls: ['./author-list.component.scss'],
 })
 export class AuthorListComponent implements OnInit {
   authors: PaginatedList<Author> | undefined;
+  paginationParams = new PaginationParams();
 
   constructor(private authorService: AuthorsService) {
+
   }
 
   ngOnInit(): void {
+    this.paginationParams.pageSize = 2;
     this.loadAuthors();
   }
 
   loadAuthors() {
-    this.authorService.getAllAuthors().subscribe({
+    this.authorService.getAllAuthors(this.paginationParams).subscribe({
       next: (response) => {
         this.authors = response;
-      }
+      },
     });
   }
 
   onPageChange(event: PageEvent) {
-
+    this.paginationParams.pageNumber = event.pageIndex + 1;
+    this.paginationParams.pageSize = event.pageSize;
+    this.loadAuthors();
   }
-
-
 }
--- client/src/app/services/authors.service.ts
+++ client/src/app/services/authors.service.ts
@@ -1,22 +1,39 @@
-import {Injectable} from '@angular/core';
-import {environment} from '../../environments/environment';
-import {PaginatedList} from '../models/paginatedList';
-import {Category} from '../models/category';
-import {HttpClient} from '@angular/common/http';
-import {CreateAuthorModel} from '../models/author/createAuthorModel';
-import {Author} from '../models/author/author';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { PaginatedList } from '../models/paginatedList';
+import { Category } from '../models/category';
+import { HttpClient } from '@angular/common/http';
+import { CreateAuthorModel } from '../models/author/createAuthorModel';
+import { Author } from '../models/author/author';
+import { PaginationParams } from '../models/paginationParams';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthorsService {
   baseUrl = environment.apiUrl;
+  authors = new Map();
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
-  getAllAuthors() {
-    return this.http.get<PaginatedList<Author>>(`${this.baseUrl}/authors`);
+  getAllAuthors(paginationParams: PaginationParams) {
+    let queryString = Object.values(paginationParams).join('-');
+    const data = this.authors.get(queryString);
+    if (data) {
+      return of(data);
+    }
+
+    return this.http
+      .get<PaginatedList<Author>>(`${this.baseUrl}/authors`, {
+        params: { ...paginationParams },
+      })
+      .pipe(
+        map((response) => {
+          this.authors.set(queryString, response);
+          return response;
+        })
+      );
   }
 
   createAuthor(createAuthorModel: CreateAuthorModel) {
