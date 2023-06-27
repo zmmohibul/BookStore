24089f02bc28d0dbaab67846e81802647353eb12
zmmohibul <18-36427-1@student.aiub.edu>
2023-06-27 06:06:49 +0600

    Category Sidebar - 4 (Works)

--- client/src/app/store/book/category-sidebar/category-sidebar.component.ts
+++ client/src/app/store/book/category-sidebar/category-sidebar.component.ts
@@ -21,7 +21,7 @@ interface Node {
 export class CategorySidebarComponent implements OnInit {
   @Output() categoryClick = new EventEmitter<number>();
 
-  allCategoryHeading: Node = { id: -1, name: 'All Categories' };
+  allCategoryHeading: Node = { id: 0, name: 'All Categories' };
   list: Node[] = [this.allCategoryHeading];
   children: Node[] = [];
 
@@ -59,7 +59,9 @@ export class CategorySidebarComponent implements OnInit {
   }
 
   onListItemClick(id: number) {
-    if (id == -1) {
+    this.categoryClick.emit(id);
+
+    if (id == 0) {
       this.children = [];
       this.loadCategories();
       return;
@@ -68,92 +70,45 @@ export class CategorySidebarComponent implements OnInit {
     for (let item of this.list) {
       newList.push(item);
       if (item.id == id) {
-        console.log(newList);
         if (this.children.length == 0 && !this.childFetched) {
           this.list = [this.allCategoryHeading, item];
         } else {
           this.list = newList;
         }
-        this.childLoading = true;
-        this.categoryService.getSubCategories(item.id).subscribe({
-          next: (response) => {
-            console.log(id);
-            console.log(item.id);
-            console.log(response);
-            if (response) {
-              this.children = response.subCategories.map((subCat: Category) => {
-                return {
-                  id: subCat.id,
-                  name: subCat.name,
-                  childrenCount: subCat.subCategoryCount,
-                };
-              });
-              this.childLoading = false;
-              console.log(this.children);
-            }
-            this.childFetched = true;
-          },
-        });
+        this.loadChildren(item.id);
         return;
       }
     }
   }
 
   onChildItemClick(id: number) {
-    console.log(id);
+    this.categoryClick.emit(id);
+
     for (let child of this.children) {
       if (child.id == id) {
         this.list.push(child);
-
-        this.categoryService.getSubCategories(child.id).subscribe({
-          next: (response) => {
-            if (response) {
-              this.children = response.subCategories.map((subCat: Category) => {
-                return {
-                  id: subCat.id,
-                  name: subCat.name,
-                  childrenCount: subCat.subCategoryCount,
-                };
-              });
-            }
-          },
-        });
-
+        this.loadChildren(child.id);
         return;
       }
     }
   }
 
-  onCategoryClick(id: number) {
-    console.log(id);
+  loadChildren(id: number) {
+    this.childLoading = true;
     this.categoryService.getSubCategories(id).subscribe({
-      next: (response) => {},
+      next: (response) => {
+        if (response) {
+          this.children = response.subCategories.map((subCat: Category) => {
+            return {
+              id: subCat.id,
+              name: subCat.name,
+              childrenCount: subCat.subCategoryCount,
+            };
+          });
+          this.childLoading = false;
+        }
+        this.childFetched = true;
+      },
     });
-    this.categoryClick.emit(id);
   }
-
-  // findNode(id: number): TreeNode | undefined {
-  //   if (this.categoryNode && this.childrenNode) {
-  //     let stack = [];
-  //     stack.push(this.categoryNode[0]);
-  //
-  //     while (stack.length > 0) {
-  //       let node = stack.pop();
-  //       if (node && node.id == id) {
-  //         return node;
-  //       }
-  //
-  //       if (node && node.children) {
-  //         for (let child of node.children) {
-  //           if (child.id == id) {
-  //             return child;
-  //           }
-  //           stack.push(child);
-  //         }
-  //       }
-  //     }
-  //   }
-  //
-  //   return undefined;
-  // }
 }
